46b1474e4e599e9c7a8837a2be3c282ee4afd6ce
Rafay <rafay.ed13@gmail.com>
2018-10-15 23:56:30 +0500

    Desktop View Almost Finished

--- js/custom.js
+++ js/custom.js
@@ -27,3 +27,36 @@ $(document).ready(function(){
   });
 });
 // Slick Slider Initializing
+
+
+// Anchor Scrolls
+$(document).ready(function() {
+  
+  var scrollLink = $('.scroll');
+  
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+  
+  // Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+    
+    scrollLink.each(function() {
+      
+      var sectionOffset = $(this.hash).offset().top - 20;
+      
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    });
+    
+  });
+  
+});
+// Anchor Scrolls
