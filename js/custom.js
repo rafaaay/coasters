5808a2e435726db8158f51cb948cf6d40b5a381e
Rafay <rafay.ed13@gmail.com>
2018-10-28 23:45:41 +0500

    Code Organization

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,57 @@
-// Header Switch On Scroll
 $(document).ready(function(){
 
-	var header = $(".primary-header");
+// Header Switch On Scroll
+var header = $(".primary-header");
 
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-    
-        if (scroll >= 100) {
-            header.removeClass("primary-header").addClass("secondary-header");
-        } else {
-            header.removeClass("secondary-header").addClass("primary-header");
-        }
-    });
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
+    if (scroll >= 100) {
+        header.removeClass("primary-header").addClass("secondary-header");
+    } else {
+        header.removeClass("secondary-header").addClass("primary-header");
+    }
 });
 // Header Switch On Scroll
 
 
 // Slick Slider Initializing
-$(document).ready(function(){
-  $('.product-slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+$('.product-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
 // Slick Slider Initializing
 
 
 // Anchor Scrolls
-$(document).ready(function() {
+var scrollLink = $('.scroll');
   
-  var scrollLink = $('.scroll');
+// Smooth scrolling
+scrollLink.click(function(e) {
+  e.preventDefault();
+  $('body,html').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 1000 );
+});
   
-  // Smooth scrolling
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
-  });
+// Active link switching
+$(window).scroll(function() {
+  var scrollbarLocation = $(this).scrollTop();
   
-  // Active link switching
-  $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
+  scrollLink.each(function() {
     
-    scrollLink.each(function() {
-      
-      var sectionOffset = $(this.hash).offset().top - 20;
-      
-      if ( sectionOffset <= scrollbarLocation ) {
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
-      }
-    });
+    var sectionOffset = $(this.hash).offset().top - 20;
     
+    if ( sectionOffset <= scrollbarLocation ) {
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
+    }
   });
   
 });
 // Anchor Scrolls
+
+
+});
